b0da29d960dda44a7aa08c391b91fdbfb7283d3f
bgyakimchuk <bgyakimchuk@gmail.com>
2020-10-11 23:05:26 -0500

    Eject from CRA, update to webpack 5

---
+++ client/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = () => {
+  const isProd = false
+
+  return {
+    devtool: isProd ? 'none' : 'source-map',
+    mode: isProd ? 'production' : 'development',
+    watch: !isProd,
+    entry: './src/index.js',
+    output: {
+      filename: isProd ? 'main.[hash].js' : 'main.js',
+      path: path.resolve('./build'),
+    },
+    devServer: {
+      compress: true,
+      port: 3000
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+          exclude: /node_modules/,
+          loader: 'file-loader',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+        favicon: './public/favicon.ico'
+      }),
+    ],
+  };
+};
